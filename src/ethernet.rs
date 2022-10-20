65dde1ac1873f670b99707b9370a2382273ef189
Michal Rostecki <vadorovsky@gmail.com>
2022-10-20 02:40:30 +0200

    ethernet: Add EthProtocol enum
    Instead of using just an u16 value for Ethernet protocols, use an enum
    where we can list known protocols.
    
    The goal is to be able to `match` on `h_proto` value instead of
    comparing with constants.
    
    Signed-off-by: Michal Rostecki <vadorovsky@gmail.com>

--- src/ethernet.rs
+++ src/ethernet.rs
@@ -3,5 +3,17 @@
 pub struct EthHdr {
     pub h_dest: [u8; 6],
     pub h_source: [u8; 6],
-    pub h_proto: u16,
+    pub h_proto: EthProtocol,
+}
+
+#[repr(u16)]
+#[derive(Debug, Copy, Clone)]
+pub enum EthProtocol {
+    Loop = 0x0060,
+    Ipv4 = 0x0800,
+    Arp = 0x0806,
+    Ipv6 = 0x86DD,
+    FibreChannel = 0x8906,
+    Infiniband = 0x8915,
+    LoopbackIeee8023 = 0x9000,
 }
