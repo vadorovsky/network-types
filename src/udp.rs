a9665170c69e38301304c3a8d38591c109b8478c
Michal Rostecki <vadorovsky@gmail.com>
2022-11-14 18:49:04 -0800

    {tcp,udp}: Add `source_from_be` and `dest_from_be` methods
    Again, to save people from doing `u16::from_be` manually, everytime.
    
    Signed-off-by: Michal Rostecki <vadorovsky@gmail.com>

--- src/udp.rs
+++ src/udp.rs
@@ -10,3 +10,17 @@ pub struct UdpHdr {
     pub len: u16,
     pub check: u16,
 }
+
+impl UdpHdr {
+    /// Returns the source port as an unsigned integer of the target's
+    /// endianness.
+    pub fn source_from_be(&self) -> u16 {
+        u16::from_be(self.source)
+    }
+
+    /// Returns the destination port as an unsigned integer of the target's
+    /// endianness.
+    pub fn dest_from_be(&self) -> u16 {
+        u16::from_be(self.dest)
+    }
+}
