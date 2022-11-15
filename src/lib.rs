1a4b81f3e491fbb7688b79771e86e373abf49643
Michal Rostecki <vadorovsky@gmail.com>
2022-11-14 23:05:28 -0800

    lib: Add the main doc page
    Signed-off-by: Michal Rostecki <vadorovsky@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! Rust structs representing network-related types (on Layer 2, 3 and 4) in
+//! Linux.
+//!
+//! The crate is [no_std](https://docs.rust-embedded.org/book/intro/no-std.html)
+//! and the structures are fully compatible with the ones provided by the Linux
+//! kernel, which makes it a great fit for [eBPF](https://ebpf.io/) programs
+//! written with [Aya](https://aya-rs.dev/).
+
 #![no_std]
 
 pub mod bitfield;
