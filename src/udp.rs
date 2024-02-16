570446d61fc95075c0290220092cdc22c662873e
Pierre Fenoll <pierrefenoll@gmail.com>
2024-02-16 14:52:18 +0100

    fix serializing UDP header struct and add test
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- src/udp.rs
+++ src/udp.rs
@@ -3,7 +3,7 @@ use core::mem;
 /// UDP header, which is present after the IP header.
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct UdpHdr {
     pub source: u16,
     pub dest: u16,
@@ -13,4 +13,23 @@ pub struct UdpHdr {
 
 impl UdpHdr {
     pub const LEN: usize = mem::size_of::<UdpHdr>();
+}
+
+#[test]
+#[cfg(feature = "serde")]
+#[cfg(feature = "bincode")]
+fn serialize() {
+    use bincode::config::standard;
+    use bincode::serde::encode_to_vec;
+
+    let udp = UdpHdr {
+        source: 4242,
+        dest: 4789,
+        len: 42,
+        check: 0,
+    };
+
+    let options = standard().with_fixed_int_encoding().with_big_endian();
+
+    encode_to_vec(&udp, options).unwrap();
 }
