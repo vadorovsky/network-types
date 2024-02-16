vadorovsky/network-types

commit ad2f15d69edf6587ede221922965fa0b5b5bc633

Add serde support to structs
Supporting serde can be useful with e.g. bincode.

src/eth.rs
@@ -3,6 +3,7 @@ use core::mem;
 /// Ethernet header, which is present at the beginning of every Ethernet frame.
 #[repr(C, packed)]
 #[derive(Debug, Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct EthHdr {
     /// Destination MAC address.
     pub dst_addr: [u8; 6],
@@ -19,6 +20,7 @@ impl EthHdr {
 /// Protocol which is encapsulated in the payload of the Ethernet frame.
 #[repr(u16)]
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub enum EtherType {
     Loop = 0x0060_u16.to_be(),
     Ipv4 = 0x0800_u16.to_be(),

src/icmp.rs
@@ -4,6 +4,7 @@ pub const ICMP_HDR_LEN: usize = mem::size_of::<IcmpHdr>();
 
 #[repr(C)]
 #[derive(Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct IcmpHdr {
     pub type_: u8,
     pub code: u8,
@@ -17,6 +18,7 @@ impl IcmpHdr {
 
 #[repr(C)]
 #[derive(Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub union IcmpHdrUn {
     pub echo: IcmpHdrEcho,
     pub gateway: u32,
@@ -26,6 +28,7 @@ pub union IcmpHdrUn {
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct IcmpHdrEcho {
     pub id: u16,
     pub sequence: u16,
@@ -33,6 +36,7 @@ pub struct IcmpHdrEcho {
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct IcmpHdrFrag {
     pub __unused: u16,
     pub mtu: u16,

src/ip.rs
@@ -3,6 +3,7 @@ use core::mem;
 use crate::bitfield::BitfieldUnit;
 
 /// IP headers, which are present after the Ethernet header.
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub enum IpHdr {
     V4(Ipv4Hdr),
     V6(Ipv6Hdr),
@@ -11,6 +12,7 @@ pub enum IpHdr {
 //// IPv4 header, which is present after the Ethernet header.
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct Ipv4Hdr {
     pub _bitfield_align_1: [u8; 0],
     pub _bitfield_1: BitfieldUnit<[u8; 1usize]>,
@@ -90,12 +92,14 @@ impl Ipv4Hdr {
 
 #[repr(C)]
 #[derive(Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct in6_addr {
     pub in6_u: in6_u,
 }
 
 #[repr(C)]
 #[derive(Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub union in6_u {
     pub u6_addr8: [u8; 16usize],
     pub u6_addr16: [u16; 8usize],
@@ -104,6 +108,7 @@ pub union in6_u {
 
 #[repr(C)]
 #[derive(Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct Ipv6Hdr {
     pub _bitfield_align_1: [u8; 0],
     pub _bitfield_1: BitfieldUnit<[u8; 1usize]>,
@@ -190,6 +195,7 @@ impl Ipv6Hdr {
 /// <https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml>
 #[repr(u8)]
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub enum IpProto {
     /// IPv6 Hop-by-Hop Option
     HopOpt = 0,

src/tcp.rs
@@ -7,6 +7,7 @@ pub const TCP_HDR_LEN: usize = mem::size_of::<TcpHdr>();
 /// TCP header, which is present after the IP header.
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct TcpHdr {
     pub source: u16,
     pub dest: u16,

src/udp.rs
@@ -3,6 +3,7 @@ use core::mem;
 /// UDP header, which is present after the IP header.
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct UdpHdr {
     pub source: u16,
     pub dest: u16,

src/vxlan.rs
@@ -5,6 +5,7 @@ use crate::bitfield::BitfieldUnit;
 /// VXLAN header, which is present at the beginning of every UDP payload containing VXLAN packets.
 #[repr(C, packed)]
 #[derive(Debug, Copy, Clone)]
+#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct VxlanHdr {
     /// VXLAN flags. See [`VxlanHdr::vni_valid`] and [`VxlanHdr::set_vni_valid`].
     pub flags: BitfieldUnit<[u8; 1usize]>,

Cargo.toml
@@ -10,6 +10,7 @@ readme = "README.md"
 edition = "2021"
 
 [dependencies]
+serde = { version = "1", default-features = false, features = ["derive"], optional = true }
 
 [dev-dependencies]
 aya-bpf = { git = "https://github.com/aya-rs/aya", branch = "main" }

src/lib.rs
@@ -87,6 +87,15 @@
 //!   * `source` -> `src`
 //!   * `destination` -> `dst`
 //!   * `address` -> `addr`
+//!
+//! # Feature flags
+//!
+//! [Serde](https://serde.rs) support can be enabled through the `serde`
+//! feature flag. It is intended to be used with binary serialization libraries
+//! like [`bincode`](https://crates.io/crates/bincode) that leverage Serde's
+//! infrastructure.
+//!
+//! Note that `no_std` support is lost when enabling Serde.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 

commit 2e8115df5e99c1ac4c349a8a03f96a894108f9a6

serde: fix cfg_attr test
Fixes: ad2f15d ("Add serde support to structs")

src/eth.rs
@@ -3,7 +3,7 @@ use core::mem;
 /// Ethernet header, which is present at the beginning of every Ethernet frame.
 #[repr(C, packed)]
 #[derive(Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct EthHdr {
     /// Destination MAC address.
     pub dst_addr: [u8; 6],
@@ -20,7 +20,7 @@ impl EthHdr {
 /// Protocol which is encapsulated in the payload of the Ethernet frame.
 #[repr(u16)]
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub enum EtherType {
     Loop = 0x0060_u16.to_be(),
     Ipv4 = 0x0800_u16.to_be(),

src/icmp.rs
@@ -4,7 +4,7 @@ pub const ICMP_HDR_LEN: usize = mem::size_of::<IcmpHdr>();
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct IcmpHdr {
     pub type_: u8,
     pub code: u8,
@@ -18,7 +18,7 @@ impl IcmpHdr {
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub union IcmpHdrUn {
     pub echo: IcmpHdrEcho,
     pub gateway: u32,
@@ -28,7 +28,7 @@ pub union IcmpHdrUn {
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct IcmpHdrEcho {
     pub id: u16,
     pub sequence: u16,
@@ -36,7 +36,7 @@ pub struct IcmpHdrEcho {
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct IcmpHdrFrag {
     pub __unused: u16,
     pub mtu: u16,

src/ip.rs
@@ -3,7 +3,7 @@ use core::mem;
 use crate::bitfield::BitfieldUnit;
 
 /// IP headers, which are present after the Ethernet header.
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub enum IpHdr {
     V4(Ipv4Hdr),
     V6(Ipv6Hdr),
@@ -12,7 +12,7 @@ pub enum IpHdr {
 //// IPv4 header, which is present after the Ethernet header.
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct Ipv4Hdr {
     pub _bitfield_align_1: [u8; 0],
     pub _bitfield_1: BitfieldUnit<[u8; 1usize]>,
@@ -96,14 +96,14 @@ impl Ipv4Hdr {
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct in6_addr {
     pub in6_u: in6_u,
 }
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub union in6_u {
     pub u6_addr8: [u8; 16usize],
     pub u6_addr16: [u16; 8usize],
@@ -112,7 +112,7 @@ pub union in6_u {
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct Ipv6Hdr {
     pub _bitfield_align_1: [u8; 0],
     pub _bitfield_1: BitfieldUnit<[u8; 1usize]>,
@@ -203,7 +203,7 @@ impl Ipv6Hdr {
 /// <https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml>
 #[repr(u8)]
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub enum IpProto {
     /// IPv6 Hop-by-Hop Option
     HopOpt = 0,

src/tcp.rs
@@ -7,7 +7,7 @@ pub const TCP_HDR_LEN: usize = mem::size_of::<TcpHdr>();
 /// TCP header, which is present after the IP header.
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct TcpHdr {
     pub source: u16,
     pub dest: u16,

src/udp.rs
@@ -3,7 +3,7 @@ use core::mem;
 /// UDP header, which is present after the IP header.
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct UdpHdr {
     pub source: u16,
     pub dest: u16,

src/vxlan.rs
@@ -5,7 +5,7 @@ use crate::bitfield::BitfieldUnit;
 /// VXLAN header, which is present at the beginning of every UDP payload containing VXLAN packets.
 #[repr(C, packed)]
 #[derive(Debug, Copy, Clone)]
-#[cfg_attr(features = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct VxlanHdr {
     /// VXLAN flags. See [`VxlanHdr::vni_valid`] and [`VxlanHdr::set_vni_valid`].
     pub flags: BitfieldUnit<[u8; 1usize]>,