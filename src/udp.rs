5e04f4fcef6d02882c770cfce9588379ace86085
Thomas Eizinger <thomas@eizinger.io>
2025-04-03 10:34:32 +1100

    refactor(udp): store fields as byte-arrays
    Within the actual network packet, this data is encoded as big-endian. By
    representing the fields as byte-arrays, we can preserve this encoding
    and the use getters and setters to correctly convert to and from the
    host endianness.
    
    Related: #32

--- src/udp.rs
+++ src/udp.rs
@@ -5,14 +5,46 @@ use core::mem;
 #[derive(Debug, Copy, Clone)]
 #[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct UdpHdr {
-    pub source: u16,
-    pub dest: u16,
-    pub len: u16,
-    pub check: u16,
+    pub source: [u8; 2],
+    pub dest: [u8; 2],
+    pub len: [u8; 2],
+    pub check: [u8; 2],
 }
 
 impl UdpHdr {
     pub const LEN: usize = mem::size_of::<UdpHdr>();
+
+    pub fn source(&self) -> u16 {
+        u16::from_be_bytes(self.source)
+    }
+
+    pub fn set_source(&mut self, source: u16) {
+        self.source = source.to_be_bytes();
+    }
+
+    pub fn dest(&self) -> u16 {
+        u16::from_be_bytes(self.dest)
+    }
+
+    pub fn set_dest(&mut self, dest: u16) {
+        self.dest = dest.to_be_bytes();
+    }
+
+    pub fn len(&self) -> u16 {
+        u16::from_be_bytes(self.len)
+    }
+
+    pub fn set_len(&mut self, len: u16) {
+        self.len = len.to_be_bytes();
+    }
+
+    pub fn check(&self) -> u16 {
+        u16::from_be_bytes(self.check)
+    }
+
+    pub fn set_check(&mut self, check: u16) {
+        self.check = check.to_be_bytes();
+    }
 }
 
 #[cfg(test)]
@@ -24,10 +56,10 @@ mod test {
         use bincode::{config::standard, serde::encode_to_vec};
 
         let udp = UdpHdr {
-            source: 4242,
-            dest: 4789,
-            len: 42,
-            check: 0,
+            source: 4242_u16.to_be_bytes(),
+            dest: 4789_u16.to_be_bytes(),
+            len: 42_u16.to_be_bytes(),
+            check: 0_u16.to_be_bytes(),
         };
 
         let options = standard().with_fixed_int_encoding().with_big_endian();
