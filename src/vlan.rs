c645c7632b52feb8fa7411941187f4a1bca615fc
Sven Cowart <svencowart@gmail.com>
2025-05-09 14:50:39 -0700

    feat: vlan tag

---
+++ src/vlan.rs
@@ -0,0 +1,40 @@
+use core::mem;
+
+/// VLAN tag header structure
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+#[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+pub struct VlanHdr {
+    /// First 2 bytes containing PCP (3 bits), DEI (1 bit), and VLAN ID (12 bits)
+    pub tci: u16,
+    /// EtherType field indicating the protocol encapsulated in the payload
+    pub eth_type: u16,
+}
+
+impl VlanHdr {
+    pub const LEN: usize = mem::size_of::<VlanHdr>();
+
+    /// Extract the Priority Code Point (PCP) from the VLAN header
+    #[inline]
+    pub fn pcp(&self) -> u8 {
+        (u16::from_be(self.tci) >> 13) as u8
+    }
+
+    /// Extract the Drop Eligible Indicator (DEI) from the VLAN header
+    #[inline]
+    pub fn dei(&self) -> u8 {
+        ((u16::from_be(self.tci) >> 12) & 1) as u8
+    }
+
+    /// Extract the VLAN ID from the VLAN header
+    #[inline]
+    pub fn vid(&self) -> u16 {
+        u16::from_be(self.tci) & 0xFFF
+    }
+    
+    /// Get the EtherType value
+    #[inline]
+    pub fn eth_type(&self) -> u16 {
+        u16::from_be(self.eth_type)
+    }
+}
