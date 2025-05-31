9c832b973e3f148eef7f7a940e025e6a7bb1643c
Badr <contact@pythops.com>
2025-05-31 13:48:43 +0200

    arp protocol

--- src/arp.rs
+++ src/arp.rs
@@ -5,11 +5,11 @@ use core::mem;
 #[derive(Debug, Copy, Clone)]
 #[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct ArpHdr {
-    pub htype: u16,
-    pub ptype: u16,
+    pub htype: [u8; 2],
+    pub ptype: [u8; 2],
     pub hlen: u8,
     pub plen: u8,
-    pub oper: u16,
+    pub oper: [u8; 2],
     pub sha: [u8; 6],
     pub spa: [u8; 4],
     pub tha: [u8; 6],
