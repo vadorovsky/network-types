22692044ea7343323e2ab61c189fa1589708ceec
Madison Grubb <madison@elastiflow.com>
2025-06-10 17:21:18 -0400

    use original var names

--- src/arp.rs
+++ src/arp.rs
@@ -5,15 +5,15 @@ use core::mem;
 #[derive(Debug, Copy, Clone)]
 #[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
 pub struct ArpHdr {
-    pub hardware_type: [u8; 2],
-    pub protocol_type: [u8; 2],
-    pub hardware_length: u8,
-    pub protocol_length: u8,
-    pub operation: [u8; 2],
-    pub sender_hardware_address: [u8; 6],
-    pub sender_protocol_address: [u8; 4],
-    pub target_hardware_address: [u8; 6],
-    pub target_protocol_address: [u8; 4],
+    pub htype: [u8; 2],
+    pub ptype: [u8; 2],
+    pub hlen: u8,
+    pub plen: u8,
+    pub oper: [u8; 2],
+    pub sha: [u8; 6],
+    pub spa: [u8; 4],
+    pub tha: [u8; 6],
+    pub tpa: [u8; 4],
 }
 
 impl ArpHdr {
@@ -21,110 +21,110 @@ impl ArpHdr {
 
     // Returns the hardware type field.
     #[inline]
-    pub fn hardware_type(&self) -> [u8; 2] {
-        self.hardware_type
+    pub fn htype(&self) -> [u8; 2] {
+        self.htype
     }
 
     // Sets the hardware type field.
     #[inline]
-    pub fn set_hardware_type(&mut self, hardware_type: [u8; 2]) {
-        self.hardware_type = hardware_type;
+    pub fn set_htype(&mut self, htype: [u8; 2]) {
+        self.htype = htype;
     }
 
     // Returns the protocol type field.
     #[inline]
-    pub fn protocol_type(&self) -> [u8; 2] {
-        self.protocol_type
+    pub fn ptype(&self) -> [u8; 2] {
+        self.ptype
     }
 
     // Sets the protocol type field.
     #[inline]
-    pub fn set_protocol_type(&mut self, protocol_type: [u8; 2]) {
-        self.protocol_type = protocol_type;
+    pub fn set_ptype(&mut self, ptype: [u8; 2]) {
+        self.ptype = ptype;
     }
 
     // Returns the hardware length field.
     #[inline]
-    pub fn hardware_length(&self) -> u8 {
-        self.hardware_length
+    pub fn hlen(&self) -> u8 {
+        self.hlen
     }
 
     // Sets the hardware length field.
     #[inline]
-    pub fn set_hardware_length(&mut self, hardware_length: u8) {
-        self.hardware_length = hardware_length;
+    pub fn set_hlen(&mut self, hlen: u8) {
+        self.hlen = hlen;
     }
 
     // Returns the protocol length field.
     #[inline]
-    pub fn protocol_length(&self) -> u8 {
-        self.protocol_length
+    pub fn plen(&self) -> u8 {
+        self.plen
     }
 
     // Sets the protocol length field.
     #[inline]
-    pub fn set_protocol_length(&mut self, protocol_length: u8) {
-        self.protocol_length = protocol_length;
+    pub fn set_plen(&mut self, plen: u8) {
+        self.plen = plen;
     }
 
-    // Returns the operation field.
+    // Returns the oper field.
     #[inline]
-    pub fn operation(&self) -> [u8; 2] {
-        self.operation
+    pub fn oper(&self) -> [u8; 2] {
+        self.oper
     }
 
-    // Sets the operation field.
+    // Sets the oper field.
     #[inline]
-    pub fn set_operation(&mut self, operation: [u8; 2]) {
-        self.operation = operation
+    pub fn set_oper(&mut self, oper: [u8; 2]) {
+        self.oper = oper
     }
 
     // Returns the sender hardware address field.
     #[inline]
-    pub fn sender_hardware_address(&self) -> [u8; 6] {
-        self.sender_hardware_address
+    pub fn sha(&self) -> [u8; 6] {
+        self.sha
     }
 
     // Sets the sender hardware address field.
     #[inline]
-    pub fn set_sender_hardware_address(&mut self, hardware_address: [u8; 6]) {
-        self.sender_hardware_address = hardware_address
+    pub fn set_sha(&mut self, hardware_address: [u8; 6]) {
+        self.sha = hardware_address
     }
 
     // Returns the sender protocol address field.
     #[inline]
-    pub fn sender_protocol_address(&self) -> [u8; 4] {
-        self.sender_protocol_address
+    pub fn spa(&self) -> [u8; 4] {
+        self.spa
     }
 
     // Sets the sender protocol address field.
     #[inline]
-    pub fn set_sender_protocol_address(&mut self, protocol_address: [u8; 4]) {
-        self.sender_protocol_address = protocol_address
+    pub fn set_spa(&mut self, protocol_address: [u8; 4]) {
+        self.spa = protocol_address
     }
 
     // Returns the target hardware address field.
     #[inline]
-    pub fn target_hardware_address(&self) -> [u8; 6] {
-        self.target_hardware_address
+    pub fn tha(&self) -> [u8; 6] {
+        self.tha
     }
 
     // Sets the target hardware address field.
     #[inline]
-    pub fn set_target_hardware_address(&mut self, hardware_address: [u8; 6]) {
-        self.target_hardware_address = hardware_address
+    pub fn set_tha(&mut self, hardware_address: [u8; 6]) {
+        self.tha = hardware_address
     }
 
     // Returns the target protocol address field.
     #[inline]
-    pub fn target_protocol_address(&self) -> [u8; 4] {
-        self.target_protocol_address
+    pub fn tpa(&self) -> [u8; 4] {
+        self.tpa
     }
 
     // Sets the target protocol address field.
     #[inline]
-    pub fn set_target_protocol_address(&mut self, protocol_address: [u8; 4]) {
-        self.target_protocol_address = protocol_address
+    pub fn set_tpa(&mut self, protocol_address: [u8; 4]) {
+        self.tpa = protocol_address
     }
 }
 
@@ -134,15 +134,15 @@ mod tests {
 
     fn default_arp_hdr() -> ArpHdr {
         ArpHdr {
-            hardware_type: [0; 2],
-            protocol_type: [0; 2],
-            hardware_length: 0,
-            protocol_length: 0,
-            operation: [0; 2],
-            sender_hardware_address: [0; 6],
-            sender_protocol_address: [0; 4],
-            target_hardware_address: [0; 6],
-            target_protocol_address: [0; 4],
+            htype: [0; 2],
+            ptype: [0; 2],
+            hlen: 0,
+            plen: 0,
+            oper: [0; 2],
+            sha: [0; 6],
+            spa: [0; 4],
+            tha: [0; 6],
+            tpa: [0; 4],
         }
     }
 
@@ -153,81 +153,81 @@ mod tests {
     }
 
     #[test]
-    fn test_hardware_type() {
+    fn test_htype() {
         let mut hdr = default_arp_hdr();
         // Test with Ethernet (value 1)
         let hw_type = 1u16.to_be_bytes();
-        hdr.set_hardware_type(hw_type);
-        assert_eq!(hdr.hardware_type(), hw_type);
+        hdr.set_htype(hw_type);
+        assert_eq!(hdr.htype(), hw_type);
     }
 
     #[test]
-    fn test_protocol_type() {
+    fn test_ptype() {
         let mut hdr = default_arp_hdr();
         // Test with IPv4 (value 0x0800)
         let proto_type = 0x0800u16.to_be_bytes();
-        hdr.set_protocol_type(proto_type);
-        assert_eq!(hdr.protocol_type(), proto_type);
+        hdr.set_ptype(proto_type);
+        assert_eq!(hdr.ptype(), proto_type);
     }
 
     #[test]
-    fn test_hardware_length() {
+    fn test_hlen() {
         let mut hdr = default_arp_hdr();
         // Test with MAC address length
-        hdr.set_hardware_length(6);
-        assert_eq!(hdr.hardware_length(), 6);
+        hdr.set_hlen(6);
+        assert_eq!(hdr.hlen(), 6);
     }
 
     #[test]
-    fn test_protocol_length() {
+    fn test_plen() {
         let mut hdr = default_arp_hdr();
         // Test with IPv4 address length
-        hdr.set_protocol_length(4);
-        assert_eq!(hdr.protocol_length(), 4);
+        hdr.set_plen(4);
+        assert_eq!(hdr.plen(), 4);
     }
 
     #[test]
-    fn test_operation() {
+    fn test_oper() {
         let mut hdr = default_arp_hdr();
         // Test with ARP Request (1)
         let op_request = 1u16.to_be_bytes();
-        hdr.set_operation(op_request);
-        assert_eq!(hdr.operation(), op_request);
+        hdr.set_oper(op_request);
+        assert_eq!(hdr.oper(), op_request);
         // Test with ARP Reply (2)
         let op_reply = 2u16.to_be_bytes();
-        hdr.set_operation(op_reply);
-        assert_eq!(hdr.operation(), op_reply);
+        hdr.set_oper(op_reply);
+        assert_eq!(hdr.oper(), op_reply);
     }
 
     #[test]
-    fn test_sender_hardware_address() {
+    fn test_sha() {
         let mut hdr = default_arp_hdr();
         let addr = [0x00, 0x11, 0x22, 0x33, 0x44, 0x55];
-        hdr.set_sender_hardware_address(addr);
-        assert_eq!(hdr.sender_hardware_address(), addr);
+        hdr.set_sha(addr);
+        assert_eq!(hdr.sha(), addr);
     }
 
     #[test]
-    fn test_sender_protocol_address() {
+    fn test_spa() {
         let mut hdr = default_arp_hdr();
         let addr = [192, 168, 1, 1];
-        hdr.set_sender_protocol_address(addr);
-        assert_eq!(hdr.sender_protocol_address(), addr);
+        hdr.set_spa(addr);
+        assert_eq!(hdr.spa(), addr);
     }
 
     #[test]
-    fn test_target_hardware_address() {
+    fn test_tha() {
         let mut hdr = default_arp_hdr();
         let addr = [0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF];
-        hdr.set_target_hardware_address(addr);
-        assert_eq!(hdr.target_hardware_address(), addr);
+        hdr.set_tha(addr);
+        assert_eq!(hdr.tha(), addr);
     }
 
     #[test]
-    fn test_target_protocol_address() {
+    fn test_tpa() {
         let mut hdr = default_arp_hdr();
         let addr = [192, 168, 1, 100];
-        hdr.set_target_protocol_address(addr);
-        assert_eq!(hdr.target_protocol_address(), addr);
+        hdr.set_tpa(addr);
+        assert_eq!(hdr.tpa(), addr);
     }
 }
