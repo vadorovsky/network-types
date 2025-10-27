1d23106cdf3755fcd137ab1c07d28cd5ea1ba411
Tamir Duberstein <tamird@gmail.com>
2025-10-27 11:05:16 -0400

    clippy: remove useless transmutes

--- src/mac.rs
+++ src/mac.rs
@@ -19,129 +19,97 @@ impl MacHdr {
 
     #[inline]
     pub fn protocol_version(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(0, 2) as u16) }
+        self._bitfield_frame_ctl.get(0, 2) as u16
     }
     #[inline]
     pub fn set_protocol_version(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(0, 2, val as u64)
-        }
+        self._bitfield_frame_ctl.set(0, 2, val.into())
     }
     #[inline]
     pub fn ty(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(2, 4) as u16) }
+        self._bitfield_frame_ctl.get(2, 4) as u16
     }
     #[inline]
     pub fn set_ty(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(2, 4, val as u64)
-        }
+        self._bitfield_frame_ctl.set(2, 4, val.into())
     }
     #[inline]
     pub fn sub_ty(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(4, 8) as u16) }
+        self._bitfield_frame_ctl.get(4, 8) as u16
     }
     #[inline]
     pub fn set_sub_ty(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(4, 8, val as u64)
-        }
+        self._bitfield_frame_ctl.set(4, 8, val.into())
     }
     #[inline]
     pub fn tds(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(8, 9) as u16) }
+        self._bitfield_frame_ctl.get(8, 9) as u16
     }
     #[inline]
     pub fn set_tds(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(8, 9, val as u64)
-        }
+        self._bitfield_frame_ctl.set(8, 9, val.into())
     }
     #[inline]
     pub fn fds(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(9, 10) as u16) }
+        self._bitfield_frame_ctl.get(9, 10) as u16
     }
     // set to from_ds
     #[inline]
     pub fn set_fds(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(9, 10, val as u64)
-        }
+        self._bitfield_frame_ctl.set(9, 10, val.into())
     }
     #[inline]
     pub fn mflag(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(10, 11) as u16) }
+        self._bitfield_frame_ctl.get(10, 11) as u16
     }
     #[inline]
     pub fn set_mflag(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(10, 11, val as u64)
-        }
+        self._bitfield_frame_ctl.set(10, 11, val.into())
     }
     #[inline]
     pub fn retry(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(11, 12) as u16) }
+        self._bitfield_frame_ctl.get(11, 12) as u16
     }
     #[inline]
     pub fn set_retry(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(11, 12, val as u64)
-        }
+        self._bitfield_frame_ctl.set(11, 12, val.into())
     }
     #[inline]
     pub fn pmgmt(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(12, 13) as u16) }
+        self._bitfield_frame_ctl.get(12, 13) as u16
     }
     // set to power_mgmt
     #[inline]
     pub fn set_pmgmt(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(12, 13, val as u64)
-        }
+        self._bitfield_frame_ctl.set(12, 13, val.into())
     }
     #[inline]
     pub fn mdata(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(13, 14) as u16) }
+        self._bitfield_frame_ctl.get(13, 14) as u16
     }
     // set to power_mgmt
     #[inline]
     pub fn set_mdata(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(13, 14, val as u64)
-        }
+        self._bitfield_frame_ctl.set(13, 14, val.into())
     }
     #[inline]
     pub fn protected_frame(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(14, 15) as u16) }
+        self._bitfield_frame_ctl.get(14, 15) as u16
     }
     #[inline]
     pub fn set_protected_frame(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(14, 15, val as u64)
-        }
+        self._bitfield_frame_ctl.set(14, 15, val.into())
     }
     #[inline]
     pub fn order(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_frame_ctl.get(15, 16) as u16) }
+        self._bitfield_frame_ctl.get(15, 16) as u16
     }
     #[inline]
     pub fn set_order(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_frame_ctl.set(15, 16, val as u64)
-        }
+        self._bitfield_frame_ctl.set(15, 16, val.into())
     }
     #[inline]
+    #[expect(clippy::too_many_arguments)]
     pub fn new_bitfield_frame_ctl(
         protocol_version: u16,
         ty: u16,
@@ -156,50 +124,17 @@ impl MacHdr {
         order: u16,
     ) -> BitfieldUnit<[u8; 2usize]> {
         let mut bitfield_unit: BitfieldUnit<[u8; 2]> = Default::default();
-        bitfield_unit.set(0, 2, {
-            let protocol_version: u16 = unsafe { ::core::mem::transmute(protocol_version) };
-            protocol_version as u64
-        });
-        bitfield_unit.set(2, 2, {
-            let ty: u16 = unsafe { ::core::mem::transmute(ty) };
-            ty as u64
-        });
-        bitfield_unit.set(4, 4, {
-            let sub_ty: u16 = unsafe { ::core::mem::transmute(sub_ty) };
-            sub_ty as u64
-        });
-        bitfield_unit.set(8, 1, {
-            let tds: u16 = unsafe { ::core::mem::transmute(tds) };
-            tds as u64
-        });
-        bitfield_unit.set(9, 1, {
-            let fds: u16 = unsafe { ::core::mem::transmute(fds) };
-            fds as u64
-        });
-        bitfield_unit.set(10, 1, {
-            let mflag: u16 = unsafe { ::core::mem::transmute(mflag) };
-            mflag as u64
-        });
-        bitfield_unit.set(11, 1, {
-            let retry: u16 = unsafe { ::core::mem::transmute(retry) };
-            retry as u64
-        });
-        bitfield_unit.set(12, 1, {
-            let pmgmt: u16 = unsafe { ::core::mem::transmute(pmgmt) };
-            pmgmt as u64
-        });
-        bitfield_unit.set(13, 1, {
-            let mdata: u16 = unsafe { ::core::mem::transmute(mdata) };
-            mdata as u64
-        });
-        bitfield_unit.set(14, 1, {
-            let protected_frame: u16 = unsafe { ::core::mem::transmute(protected_frame) };
-            protected_frame as u64
-        });
-        bitfield_unit.set(15, 1, {
-            let order: u16 = unsafe { ::core::mem::transmute(order) };
-            order as u64
-        });
+        bitfield_unit.set(0, 2, protocol_version.into());
+        bitfield_unit.set(2, 2, ty.into());
+        bitfield_unit.set(4, 4, sub_ty.into());
+        bitfield_unit.set(8, 1, tds.into());
+        bitfield_unit.set(9, 1, fds.into());
+        bitfield_unit.set(10, 1, mflag.into());
+        bitfield_unit.set(11, 1, retry.into());
+        bitfield_unit.set(12, 1, pmgmt.into());
+        bitfield_unit.set(13, 1, mdata.into());
+        bitfield_unit.set(14, 1, protected_frame.into());
+        bitfield_unit.set(15, 1, order.into());
         bitfield_unit
     }
 }
--- src/tcp.rs
+++ src/tcp.rs
@@ -25,115 +25,86 @@ impl TcpHdr {
 
     #[inline]
     pub fn res1(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(0usize, 4u8) as u16) }
+        self._bitfield_1.get(0usize, 4u8) as u16
     }
     #[inline]
     pub fn set_res1(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(0usize, 4u8, val as u64)
-        }
+        self._bitfield_1.set(0usize, 4u8, val.into())
     }
     #[inline]
     pub fn doff(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(4usize, 4u8) as u16) }
+        self._bitfield_1.get(4usize, 4u8) as u16
     }
     #[inline]
     pub fn set_doff(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(4usize, 4u8, val as u64)
-        }
+        self._bitfield_1.set(4usize, 4u8, val.into())
     }
     #[inline]
     pub fn fin(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(8usize, 1u8) as u16) }
+        self._bitfield_1.get(8usize, 1u8) as u16
     }
     #[inline]
     pub fn set_fin(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(8usize, 1u8, val as u64)
-        }
+        self._bitfield_1.set(8usize, 1u8, val.into())
     }
     #[inline]
     pub fn syn(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(9usize, 1u8) as u16) }
+        self._bitfield_1.get(9usize, 1u8) as u16
     }
     #[inline]
     pub fn set_syn(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(9usize, 1u8, val as u64)
-        }
+        self._bitfield_1.set(9usize, 1u8, val.into())
     }
     #[inline]
     pub fn rst(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(10usize, 1u8) as u16) }
+        self._bitfield_1.get(10usize, 1u8) as u16
     }
     #[inline]
     pub fn set_rst(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(10usize, 1u8, val as u64)
-        }
+        self._bitfield_1.set(10usize, 1u8, val.into())
     }
     #[inline]
     pub fn psh(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(11usize, 1u8) as u16) }
+        self._bitfield_1.get(11usize, 1u8) as u16
     }
     #[inline]
     pub fn set_psh(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(11usize, 1u8, val as u64)
-        }
+        self._bitfield_1.set(11usize, 1u8, val.into())
     }
     #[inline]
     pub fn ack(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(12usize, 1u8) as u16) }
+        self._bitfield_1.get(12usize, 1u8) as u16
     }
     #[inline]
     pub fn set_ack(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(12usize, 1u8, val as u64)
-        }
+        self._bitfield_1.set(12usize, 1u8, val.into())
     }
     #[inline]
     pub fn urg(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(13usize, 1u8) as u16) }
+        self._bitfield_1.get(13usize, 1u8) as u16
     }
     #[inline]
     pub fn set_urg(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(13usize, 1u8, val as u64)
-        }
+        self._bitfield_1.set(13usize, 1u8, val.into())
     }
     #[inline]
     pub fn ece(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(14usize, 1u8) as u16) }
+        self._bitfield_1.get(14usize, 1u8) as u16
     }
     #[inline]
     pub fn set_ece(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(14usize, 1u8, val as u64)
-        }
+        self._bitfield_1.set(14usize, 1u8, val.into())
     }
     #[inline]
     pub fn cwr(&self) -> u16 {
-        unsafe { ::core::mem::transmute(self._bitfield_1.get(15usize, 1u8) as u16) }
+        self._bitfield_1.get(15usize, 1u8) as u16
     }
     #[inline]
     pub fn set_cwr(&mut self, val: u16) {
-        unsafe {
-            let val: u16 = ::core::mem::transmute(val);
-            self._bitfield_1.set(15usize, 1u8, val as u64)
-        }
+        self._bitfield_1.set(15usize, 1u8, val.into())
     }
     #[inline]
+    #[expect(clippy::too_many_arguments)]
     pub fn new_bitfield_1(
         res1: u16,
         doff: u16,
@@ -147,46 +118,16 @@ impl TcpHdr {
         cwr: u16,
     ) -> BitfieldUnit<[u8; 2usize]> {
         let mut bitfield_unit: BitfieldUnit<[u8; 2usize]> = Default::default();
-        bitfield_unit.set(0usize, 4u8, {
-            let res1: u16 = unsafe { ::core::mem::transmute(res1) };
-            res1 as u64
-        });
-        bitfield_unit.set(4usize, 4u8, {
-            let doff: u16 = unsafe { ::core::mem::transmute(doff) };
-            doff as u64
-        });
-        bitfield_unit.set(8usize, 1u8, {
-            let fin: u16 = unsafe { ::core::mem::transmute(fin) };
-            fin as u64
-        });
-        bitfield_unit.set(9usize, 1u8, {
-            let syn: u16 = unsafe { ::core::mem::transmute(syn) };
-            syn as u64
-        });
-        bitfield_unit.set(10usize, 1u8, {
-            let rst: u16 = unsafe { ::core::mem::transmute(rst) };
-            rst as u64
-        });
-        bitfield_unit.set(11usize, 1u8, {
-            let psh: u16 = unsafe { ::core::mem::transmute(psh) };
-            psh as u64
-        });
-        bitfield_unit.set(12usize, 1u8, {
-            let ack: u16 = unsafe { ::core::mem::transmute(ack) };
-            ack as u64
-        });
-        bitfield_unit.set(13usize, 1u8, {
-            let urg: u16 = unsafe { ::core::mem::transmute(urg) };
-            urg as u64
-        });
-        bitfield_unit.set(14usize, 1u8, {
-            let ece: u16 = unsafe { ::core::mem::transmute(ece) };
-            ece as u64
-        });
-        bitfield_unit.set(15usize, 1u8, {
-            let cwr: u16 = unsafe { ::core::mem::transmute(cwr) };
-            cwr as u64
-        });
+        bitfield_unit.set(0usize, 4u8, res1.into());
+        bitfield_unit.set(4usize, 4u8, doff.into());
+        bitfield_unit.set(8usize, 1u8, fin.into());
+        bitfield_unit.set(9usize, 1u8, syn.into());
+        bitfield_unit.set(10usize, 1u8, rst.into());
+        bitfield_unit.set(11usize, 1u8, psh.into());
+        bitfield_unit.set(12usize, 1u8, ack.into());
+        bitfield_unit.set(13usize, 1u8, urg.into());
+        bitfield_unit.set(14usize, 1u8, ece.into());
+        bitfield_unit.set(15usize, 1u8, cwr.into());
         bitfield_unit
     }
 }
